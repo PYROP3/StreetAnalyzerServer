1383cdacfa8e79a48f58fcd36e51ded1738153d5
PYROP3 <4544085+PYROP3@users.noreply.github.com>
2020-06-07 18:49:32 -0300

    Using salted SHA256 of password

--- util/Util.js
+++ util/Util.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Constants = require("./Constants.js");
+const crypto = require('crypto');
 require('dotenv').config({path: __dirname + '/.env'});
 
 /**
@@ -57,3 +58,15 @@ module.exports.isLocalEnvironment = (process.env.NODE_ENV === 'local');
  * Returns server url
  */
 module.exports.serverUrl = (process.env.SERVER_URL != null ? process.env.SERVER_URL : Constants.SERVER_URL_DEFAULT)
+
+/**
+ * Return a salted hash created from username and password
+ *
+ * @param username {String} Username (primary key)
+ * @param password {String} Password
+ */
+module.exports.saltAndHashPassword = function(username, password) {
+    return crypto.createHmac('sha256', process.env.PASSWORD_HASHING_SECRET)
+                   .update(username + "." + password)
+                   .digest('hex');
+}
