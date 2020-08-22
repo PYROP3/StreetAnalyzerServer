eae4cce7dd03f2270841712c5e182b7d6cf5aff0
PYROP3 <4544085+PYROP3@users.noreply.github.com>
2020-08-22 20:44:18 -0300

    Implemented route API

--- util/Constants.js
+++ util/Constants.js
@@ -2,6 +2,7 @@
 // -- API
 module.exports.QUALITY_OVERLAY_REQUEST = "/qualityOverlay";
 module.exports.LOG_TRIP_REQUEST = "/logTrip";
+module.exports.ROUTE_REQUEST = "/route";
 // -- Account
 module.exports.CREATE_ACCOUNT_REQUEST = "/createAccount";
 module.exports.VERIFY_ACCOUNT_REQUEST = "/verifyAccount";
@@ -12,6 +13,7 @@ module.exports.DEAUTH_REQUEST         = "/deauth";
 module.exports.SCRIPT_PATH = "script/"
 module.exports.SCRIPT_SLICE_OVERLAY = module.exports.SCRIPT_PATH + "sliceOverlay.py"
 module.exports.SCRIPT_LOG_TRIP      = module.exports.SCRIPT_PATH + "logTrip.py"
+module.exports.SCRIPT_ROUTE         = module.exports.SCRIPT_PATH + "route.py"
 
 // Log storage
 module.exports.LOG_STORAGE_PATH = "log/"
@@ -40,3 +42,116 @@ module.exports.AUTH_TOKEN_NAME = module.exports.AUTH_TOKEN_TYPE + " "
 
 module.exports.SERVER_PORT_DEFAULT = 8080
 module.exports.SERVER_URL_DEFAULT = "http://localhost:"+module.exports.SERVER_PORT_DEFAULT
+
+module.exports.QUALITY_DATA_TAG = "quality"
+
+// TODO remove before deploy
+module.exports.MOCK_DIRECTIONS_RESPONSE = {
+    "status": "OK",
+    "geocoded_waypoints": [
+        {
+            "geocoder_status": "OK",
+            "place_id": "ChIJ7cv00DwsDogRAMDACa2m4K8",
+            "types": [
+                "locality",
+                "political"
+            ]
+        },
+        {
+            "geocoder_status": "OK",
+            "place_id": "ChIJ69Pk6jdlyIcRDqM1KDY3Fpg",
+            "types": [
+                "locality",
+                "political"
+            ]
+        },
+        {
+            "geocoder_status": "OK",
+            "place_id": "ChIJgdL4flSKrYcRnTpP0XQSojM",
+            "types": [
+                "locality",
+                "political"
+            ]
+        },
+        {
+            "geocoder_status": "OK",
+            "place_id": "ChIJE9on3F3HwoAR9AhGJW_fL-I",
+            "types": [
+                "locality",
+                "political"
+            ]
+        }
+    ],
+    "routes": [
+        {
+            "summary": "I-40 W",
+            "legs": [
+                {
+                    "steps": [
+                        {
+                            "travel_mode": "DRIVING",
+                            "start_location": {
+                                "lat": 41.85073,
+                                "lng": -87.65126
+                            },
+                            "end_location": {
+                                "lat": 41.85258,
+                                "lng": -87.65141
+                            },
+                            "polyline": {
+                                "points": "a~l~Fjk~uOwHJy@P"
+                            },
+                            "duration": {
+                                "value": 19,
+                                "text": "1 min"
+                            },
+                            "html_instructions": "Head <b>north</b> on <b>S Morgan St</b> toward <b>W Cermak Rd</b>",
+                            "distance": {
+                                "value": 207,
+                                "text": "0.1 mi"
+                            }
+                        }
+                    ],
+                    "duration": {
+                        "value": 74384,
+                        "text": "20 hours 40 mins"
+                    },
+                    "distance": {
+                        "value": 2137146,
+                        "text": "1,328 mi"
+                    },
+                    "start_location": {
+                        "lat": 35.4675602,
+                        "lng": -97.5164276
+                    },
+                    "end_location": {
+                        "lat": 34.0522342,
+                        "lng": -118.2436849
+                    },
+                    "start_address": "Oklahoma City, OK, USA",
+                    "end_address": "Los Angeles, CA, USA"
+                }
+            ],
+            "copyrights": "Map data ©2010 Google, Sanborn",
+            "overview_polyline": {
+                "points": "a~l~Fjk~uOnzh@vlbBtc~@tsE`vnApw{A`dw@~w\\|tNtqf@l{Yd_Fblh@rxo@b}@xxSfytAblk@xxaBeJxlcBb~t@zbh@jc|Bx}C`rv@rw|@rlhA~dVzeo@vrSnc}Axf]fjz@xfFbw~@dz{A~d{A|zOxbrBbdUvpo@`cFp~xBc`Hk@nurDznmFfwMbwz@bbl@lq~@loPpxq@bw_@v|{CbtY~jGqeMb{iF|n\\~mbDzeVh_Wr|Efc\\x`Ij{kE}mAb~uF{cNd}xBjp]fulBiwJpgg@|kHntyArpb@bijCk_Kv~eGyqTj_|@`uV`k|DcsNdwxAott@r}q@_gc@nu`CnvHx`k@dse@j|p@zpiAp|gEicy@`omFvaErfo@igQxnlApqGze~AsyRzrjAb__@ftyB}pIlo_BflmA~yQftNboWzoAlzp@mz`@|}_@fda@jakEitAn{fB_a]lexClshBtmqAdmY_hLxiZd~XtaBndgC"
+            },
+            "warnings": [],
+            "waypoint_order": [
+                0,
+                1
+            ],
+            "bounds": {
+                "southwest": {
+                    "lat": 34.05236,
+                    "lng": -118.24356
+                },
+                "northeast": {
+                    "lat": 41.87811,
+                    "lng": -87.62979
+                }
+            }
+        }
+    ]
+}
+  
--- util/Util.js
+++ util/Util.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const fs = require('fs');
 const Constants = require("./Constants.js");
+const crypto = require('crypto');
+const logger = require("./Logger.js").logger;
+const https = require('https');
 require('dotenv').config({path: __dirname + '/.env'});
 
 /**
@@ -75,3 +78,87 @@ module.exports.isLocalEnvironment = (process.env.NODE_ENV === 'local');
  * Returns server url
  */
 module.exports.serverUrl = (process.env.SERVER_URL != null ? process.env.SERVER_URL : Constants.SERVER_URL_DEFAULT)
+
+/**
+ * Return a salted hash created from username and password
+ *
+ * @param username {String} Username (primary key)
+ * @param password {String} Password
+ */
+module.exports.saltAndHashPassword = function(username, password) {
+    return crypto.createHmac('sha256', process.env.PASSWORD_HASHING_SECRET)
+                   .update(username + "." + password)
+                   .digest('hex');
+}
+
+/**
+ * Insert variables inside strings
+ *
+ * @param str {String} String to use as base
+ * @param args* {String} Parameters to be inserted inside str
+ */
+module.exports.format = function (str) {
+    var args = [].slice.call(arguments, 1),
+        i = 0;
+
+    return str.replace(/%s/g, () => args[i++]);
+}
+
+/**
+ * Make a request to a given URL (synchronous)
+ *
+ * @param reqUrl {String} URL of GET request
+ */
+module.exports.request = function (reqUrl) {
+    return new Promise((resolve, reject) => {
+        let responseBuffer = "";
+        https.get(reqUrl, (resp) => {
+            // A chunk of data has been received.
+            resp.on('data', (chunk) => {
+                //logger.debug("Got data: " + chunk)
+                responseBuffer += chunk;
+            });
+    
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                //logger.debug("Request done!", JSON.parse(responseBuffer));
+                resolve(responseBuffer);
+            });
+    
+        }).on("error", (err) => {
+            //logger.error("Error: " + err.message);
+            reject(err);
+        });
+    });
+}
+
+/**
+ * Get a single message from an IPC message queue
+ *
+ * @param queue {posixmq} message queue to fetch the message
+ */
+module.exports.getMessageFromQueue = function (queue) {
+    console.log("Getting msg")
+    return new Promise((resolve, reject) => {
+        let n;
+        let readbuf = Buffer.alloc(queue.msgsize);
+        let waiting = true;
+
+        queue.on('messages', () => {
+            if (waiting) {
+                let msg;
+                n = queue.shift(readbuf);
+                if (n === false) {
+                    reject("No messages");
+                }
+                msg = readbuf.toString('utf8', 0, n);
+                console.log("Received message("+ n +" bytes): " + msg);
+                waiting = false;
+                // Reset events
+                queue.on('messages', () => {});
+                resolve(msg);
+            }
+            reject("Not waiting");
+        });
+    });
+}
